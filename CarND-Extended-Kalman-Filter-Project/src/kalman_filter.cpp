4c2d59baababb8860f5692e857ba06fe72531e12
Daniel Kohlsdorf <daniel.kohlsdorf@xing.com>
2020-03-22 20:30:41 +0100

    adding kalman code

--- CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -1,12 +1,8 @@
 #include "kalman_filter.h"
+#include <math.h>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
-
-/* 
- * Please note that the Eigen library does not initialize 
- *   VectorXd or MatrixXd objects with zeros upon creation.
- */
 
 KalmanFilter::KalmanFilter() {}
 
@@ -14,28 +10,52 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+  x_ = x_in; 	// object state
+  P_ = P_in;  // object covariance matrix
+  F_ = F_in;  // state transition matrix
+  H_ = H_in;  // measurement matrix
+  R_ = R_in;  // measurement covariance matrix
+  Q_ = Q_in;  // process covariance matrix
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+  VectorXd y  = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S  = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+  x_ = x_ + (K * y);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+  VectorXd h = VectorXd(3);
+  h << 
+    sqrt(x_(0) * x_(0) + x_(1) * x_(1)), 
+    atan2(x_(1), x_(1)), 
+    (x_(0) * x_(2) + x_(1) * x_(3)) / sqrt(x_(0) * x_(0) + x_(1) * x_(1));
+  VectorXd y = z - h;
+  y(1) = normalize_angle(y(1));
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S  = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+  x_ = x_ + (K * y);
+  P_ = (I - K * H_) * P_;
+}
+
+double normalize_angle(double angle) {
+  while(angle < -M_PI) {
+    angle += 2 * M_PI;
+  }
+  while(angle > M_PI) {
+    angle -= 2 * M_PI;
+  }
+  return angle;
 }
--- CarND-Extended-Kalman-Filter-Project/src/tools.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/tools.cpp
@@ -1,8 +1,8 @@
 #include "tools.h"
 #include <iostream>
 
-using Eigen::VectorXd;
 using Eigen::MatrixXd;
+using Eigen::VectorXd;
 using std::vector;
 
 Tools::Tools() {}
@@ -10,15 +10,57 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+                              const vector<VectorXd> &ground_truth)
+{
+   assert(estimations.size() > 0);
+   assert(estimations.size() == ground_truth.size);
+   assert(estimations[0].size() == ground_truth[0].size);
+
+   int d = estimations[0].size;
+   VectorXD rmse(d);
+   for (int i = 0; i < d; i++)
+   {
+      rmse(i) = 0;
+   }
+
+   for (int i = 0; i < estimations.size(); i++)
+   {
+      VectorXD err = ground_truth[i] - estimations[i];
+      err = err.array() * err.array();
+      rmse += err;
+   }
+   rmse /= estimations.size();
+   rmse = rmse.array().sqrt();
+   return rmse;
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state)
+{
+   float position_x = x_state(0);
+   float position_y = x_state(1);
+   float velocity_x = x_state(2);
+   float velocity_y = x_state(3);
+
+   float scaler1 = px * px + py * py;
+   float scaler2 = sqrt(c1);
+   float scaler3 = (c1 * c2);
+
+   MatrixXd Hj(3, 4);
+   if (fabs(c1) < 0.0001)
+   {
+      cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+      return Hj;
+   }
+   float h00 = (position_x / scaler2);
+   float h01 = (position_y / scaler2);
+   float h10 = -(position_y / scaler1);
+   float h11 = (position_x / scaler1);
+   float h20 = position_y * (velocity_x * position_y - velocity_y * position_x) / scaler3;
+   float h21 = position_x * (position_x * velocity_y - position_y * velocity_x) / scaler3;
+   float h22 = position_x / scaler2;
+   float h23 = position_y / scaler2;
+   Hj << h00, h00, 0, 0,
+       h10, h11, 0, 0,
+       h20, h21, h22, h23;
+   return Hj;
 }
