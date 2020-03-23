18ada4156d85086d8426e6b69094af9f226b2a92
Daniel Kohlsdorf <daniel.kohlsdorf@xing.com>
2020-03-23 21:49:40 +0100

    working on kalman

--- CarND-Extended-Kalman-Filter-Project/src/FusionEKF.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/FusionEKF.cpp
@@ -23,20 +23,37 @@ FusionEKF::FusionEKF() {
   H_laser_ = MatrixXd(2, 4);
   Hj_      = MatrixXd(3, 4);
 
-  //measurement covariance matrix - laser
   R_laser_ << 0.0225, 0,
               0, 0.0225;
 
-  //measurement covariance matrix - radar
   R_radar_ << 0.09, 0, 0,
               0, 0.0009, 0,
               0, 0, 0.09;
 
-  /**
-   * TODO: Finish initializing the FusionEKF.
-   * TODO: Set the process and measurement noises
-   */
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
 
+  kf_.x_ = VectorXd(4);
+
+  kf_.P_ = MatrixXd(4, 4);
+  kf_.P_ << 1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1000, 0,
+            0, 0, 0, 1000;
+
+  kf_.R_ = MatrixXd(2, 2);
+  kf_.R_ << 0.0225, 0,
+            0, 0.0225;
+
+  kf_.H_ = MatrixXd(2, 4);
+  kf_.H_ << 1, 0, 0, 0,
+            0, 1, 0, 0;
+
+  kf_.F_ = MatrixXd(4, 4);
+  kf_.F_ << 1, 0, 1, 0,
+            0, 1, 0, 1,
+            0, 0, 1, 0,
+            0, 0, 0, 1;
 
 }
 
